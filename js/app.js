aeb28ce636a822b2a9adeb349426dbe13d444601
dawgavins <dawgavins@users.noreply.github.com>
2015-03-15 19:21:22 -0700

    Actually check in the modified game files.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+var MIN_ENEMY_SPEED = 101 / 1.25; // 1.25 seconds per tile
+var MAX_ENEMY_SPEED = 101 / 0.3; // .3 seconds per tile 
+var ENEMY_WIDTH = 101;
+var PLAYER_WIDTH = 60;
+var PLAYER_SIDE_SPACE = 25;
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,14 +12,116 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // x and y are the coordinates of the enemy on the canvas
+    this.x = 0;
+    this.y = 0;
+
+    // row is an integer index representing which row of tiles the enemy is moving on.
+    // the enemy's y value can be calculated directly from the row its on
+    this.row = 0;
+
+    // speed is the number of tiles the enemy will move per second
+    this.speed = 0;
+
+    // randomize the enemies position and start him moving across the screen
+    this.reset();
+}
+
+Enemy.prototype.reset = function()
+{
+    // Choose a row for the enemy to be started on. 
+    // First we will check if there are any enemies in the player's current row.  If not
+    // this enemy will go down this row.  Otherwise we will choose a random row, taking care 
+    // not to put more than two enemies in a row at the same time.
+
+    var newRow = -1; // the row the enemy will be placed in
+    var playerRowCount = 0; // used to keep track of # of enemies in the player's current row
+    var playerRow = player.row; // the player's current row
+
+    // check if there are any enemies currently in the player's row
+    for ( i in allEnemies )
+    {
+        if ( allEnemies[i].row === playerRow )
+        {
+            // there is already an enemy in the player's row
+            playerRowCount++;
+        }
+    }    
+    // if there aren't any enemies in the player's row, put this one there for sure!
+    //  keep that player moving!
+    if ( playerRowCount == 0 )
+    {
+        newRow = playerRow;
+    }
+    else
+    {
+        // Otherwise, pick a random row.  However, if there are already 2 enemies
+        // on that row, then don't put this enemy into the same one.
+        // Note - there is no chance of an infinite while loop here because there are only 4 
+        // enemies - not enough to have 2 per row.  These numbers should really be put into
+        // tweaker variables rather than hard-coded, but the project is simple enough 
+        // to do this directly
+
+        var rowCount = 5; // the number of enemies in the currently selected row
+
+        // repeat until we have a row with less than two other enemies
+        while ( rowCount >= 2)
+        {
+            rowCount = 0;
+            // select a random row other than the water row, and then check how many enemies are on it
+            newRow = Math.floor(Math.random()*4) + 1;
+            for ( i in allEnemies )
+            {
+                // don't count ourselves as belonging to any row just now
+                if ( allEnemies[i] !== this )
+                {
+                    // add 1 to the rowCount if we find an enemy in the proposed row
+                    if ( allEnemies[i].row === newRow )
+                    {
+                        rowCount++;
+                    }
+                }
+            }        
+        }   
+    }
+    // assign the result of the row search and the initial position of the enemy based on that
+    this.row = newRow;
+    this.x = -101;
+    this.y = this.row * 83 - 25;
+    // randomly generate a speed in tiles/second, between the constants min/max enemy speed
+    this.speed = Math.random() * (MAX_ENEMY_SPEED - MIN_ENEMY_SPEED) + MIN_ENEMY_SPEED;
+}
+
+// return true if this enemy has collided with the player, and false otherwise
+Enemy.prototype.isCollidingWithPlayer = function() {
+    // to collide the enemy must be in the same row, and overlap the player's horizontal position
+    // Note:  I added some non-colliding space on either side of the player (PLAYER_SIDE_SPACE)
+    // because he doesn't occupy a full tile width.
+    if ( this.row === player.row && this.x + ENEMY_WIDTH > (player.x + PLAYER_SIDE_SPACE) && this.x < ( player.x + 101 - PLAYER_SIDE_SPACE ) ) {
+        return true;
+    }
+    return false;
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+
+    // if the enemy is colliding with the player, set the lostGame flag
+    if ( this.isCollidingWithPlayer() ) {
+        player.lostGame = true;
+    }
+
+    // if the enemy is off the screen to the right, reset it 
+    // so that it will begin on the left again
+    if ( this.x > 101 * 5 ) {
+        this.reset();
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -21,16 +129,110 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+// Player class
+
+// Enemies our player must avoid
+var Player = function() {
+ 
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+
+    // row is an integer indicating which tile layer the player is occupying
+    this.row = 5; 
+
+    // x, y represent the position of the player on the canvas
+    this.x = this.column * 101;
+    this.y = this.row * 83 - 25;
+
+    // wonGame and lostGame are flags used to indicate when the game has been
+    // won or lost
+    this.wonGame = false;
+    this.lostGame = false;
+
+    this.reset();
+}
+
+Player.prototype.reset = function()
+{
+    // reset the player position to the starting tile
+    this.column = 2;
+    this.row = 5;
+    this.x = this.column * 101;
+    this.y = this.row * 83 - 25;
+    // set the end-game flags to false
+    this.wonGame = false;
+    this.lostGame = false;
+}
+
+// Update the player's position, required method for game
+// Parameter: dt, a time deltahighScore between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// read the user input so that we can make the player react accordingly
+Player.prototype.handleInput = function(keyPressed) {
+    // read the keyPressed and increment or decrement the player's row/column members
+    // while making sure they stay within the boundaries of the game
+
+    if ( keyPressed === "left" ) {
+        if ( this.column > 1  )
+        {
+           this.column -= 1;
+           currentScore += 1;
+        }
+    }
+    else if ( keyPressed === "right" ) {
+        if ( this.column < 4 )
+        {
+           this.column += 1;
+           currentScore += 1;
+        }
+    }
+    else if ( keyPressed === "up" ) {
+        this.row -= 1;
+        currentScore += 1;
+        // if the player has reached row 0, they have won the game!
+        if ( this.row < 1 ) {
+            this.wonGame = 1;
+            currentScore += 50;
+        }
+    }
+    else if ( keyPressed === "down" ) {
+        if ( this.row < 5 ) {
+            this.row += 1;
+            currentScore += 1;
+        }
+    }
+
+    // convert the row/column values to canvas x/y coordinates
+    this.x = this.column * 101;
+    this.y = this.row * 83 - 25;
+}
 
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
+// the number of enemies active at a time
+var NUM_ENEMIES = 4;
 
+// create the player
+var player = new Player();
+
+// create an array of NUM_ENEMIES enemies
+var allEnemies = [];
+for ( var i = 0; i < NUM_ENEMIES; ++i ) 
+{
+    allEnemies[i] = new Enemy();
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -42,5 +244,10 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    // only send this keyPressed if the game is in a running state
+    // i.e. don't move the player if we are in the results screen
+    if ( gameState === "running" )
+    {
+        player.handleInput(allowedKeys[e.keyCode]);
+    }
 });
--- js/engine.js
+++ js/engine.js
@@ -14,6 +14,150 @@
  * a little simpler to work with.
  */
 
+
+/* This is a helper class that facilitates drawing elements to a canvas once
+   and then rendering the canvas onto the main canvas.  I added this because I 
+   found that rendering text with fillText every frame really slowed down the game */
+function CanvasImage(width, height)
+{
+    // create a new canvas element of requested dimensions
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = width;
+    this.canvas.height = height;
+    // store the context for ease of access
+    this.ctx = this.canvas.getContext('2d');
+    // flag the canvas to be redrawn the first time
+    this.forceDraw = true;
+}
+/* function for forcing the results to be redrawn.  Likely called when 
+   there is a new high score */
+CanvasImage.prototype.setForceDraw = function() { 
+    // set the forceDraw flag to true
+    this.forceDraw = true; 
+}
+/* return true if the canvas should be redrawn before rendering, 
+   false if its already up to date */
+CanvasImage.prototype.shouldDrawToCanvas = function() {
+    return this.forceDraw;
+}
+/* draw the contents of the canvas to the input parameter "context" at position
+   x,y.  Check if the canvas should be redrawn first */
+CanvasImage.prototype.draw = function(context, x, y) {
+    // this is a 
+    if ( this.shouldDrawToCanvas() )
+    {
+        this.drawToCanvas();
+        this.forceDraw = false;
+
+    }
+    context.drawImage(this.canvas, x, y);
+}
+/* this function will be overridden by subclasses to actually draw
+   to the canvas */
+CanvasImage.prototype.drawToCanvas = function() {}
+
+
+/* Subclass of CanvasImage that draws the background for the scoreboard
+   This class only needs to redrawn the first time because it stays the same
+   at all times */
+function ScoreboardBG(width, height) {
+    CanvasImage.call(this, width, height);
+}
+ScoreboardBG.prototype = Object.create(CanvasImage.prototype);
+ScoreboardBG.prototype.constructor = ScoreboardBG;
+/* draw a rectangle with a stroke as the background for the scoreboard,
+   and render the static text as well */
+ScoreboardBG.prototype.drawToCanvas = function() {
+    // draw the rectangle with stroke border
+    this.ctx.rect(0, 0, this.canvas.width, this.canvas.height );
+    this.ctx.fillStyle = '#ddd'; // gray fill color
+    this.ctx.fill();
+    this.ctx.lineWidth = 7;
+    this.ctx.strokeStyle = '#44a'; // blue border
+    this.ctx.stroke();
+
+    // draw the static text for the score and the high score which
+    // does not change during the current game
+    var highScoreText = "High:  " + highScore;
+    this.ctx.font = "30pt Impact";
+    this.ctx.textAlign = "left";
+    this.ctx.fillStyle = "#44a"; // blue text
+    this.ctx.fillText( highScoreText, 20, 42 );    
+    this.ctx.fillText( "Score:", this.canvas.width - 220, 42 );    
+}
+
+/* Subclass of CanvasImage that draws the foreground for the scoreboard
+   This class needs to be redrawn every time the score changes. */
+function ScoreboardFG(width, height) {
+    CanvasImage.call(this, width, height);
+    this.drawnScore = 0;
+}
+ScoreboardFG.prototype = Object.create(CanvasImage.prototype);
+ScoreboardFG.prototype.constructor = ScoreboardFG;
+// overridden function to check for a redraw if the score has changed
+// or if its the first time through.
+ScoreboardFG.prototype.shouldDrawToCanvas = function() {
+    return this.forceDraw || this.drawnScore != currentScore;
+}
+// draw the text of the current score to the canvas
+ScoreboardFG.prototype.drawToCanvas = function() {
+    // clear the canvas first
+    this.ctx.clearRect( 0, 0, this.canvas.width, this.canvas.height );
+    this.ctx.font = "30pt Impact";
+    this.ctx.textAlign = "left";
+    this.ctx.fillStyle = "#44a"; // blue text
+    this.ctx.fillText( currentScore, 10, 37 );
+
+    // store the latest score drawn
+    this.drawnScore = currentScore;
+}
+
+/* Subclass of CanvasImage that draws the results screen.  I do this
+   to avoid lots of fillText calls which slow the game down */
+function ResultsScreen(width, height) {
+    CanvasImage.call(this, width, height);
+}
+ResultsScreen.prototype = Object.create(CanvasImage.prototype);
+ResultsScreen.prototype.constructor = ResultsScreen;
+// Render the text for the results screen.
+ResultsScreen.prototype.drawToCanvas = function() {
+    // clear the canvas first
+    this.ctx.clearRect( 0, 0, this.canvas.width, this.canvas.height );
+    // Initialize main result text to the non-high score, getting-smushed case
+    var resultText = "Smushed!";
+    var promptText = "Press space bar to play again";
+
+    // if the player got a high score, show congratulatory text
+    if ( Number(currentScore) > Number(highScore) ) {
+        resultText = "NEW HIGH SCORE:  " + currentScore;
+        showHighScoreText = false;
+    }
+    // if they made it across, give them some props, even if it wasn't a record
+    else if ( player.wonGame ) {
+        resultText =  "You Made It!";
+    }
+
+    // show the result in large text in the center of the screen
+    // white text with a black stroke
+    this.ctx.font = "42pt Impact";
+    this.ctx.textAlign = "center";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText( resultText, this.canvas.width / 2, this.canvas.height / 2 );
+    this.ctx.strokeStyle = "black";
+    this.ctx.lineWidth = 3;
+    this.ctx.strokeText( resultText, this.canvas.width / 2, this.canvas.height / 2 );        
+
+    // prompt the user to press space again to restart
+    // white text with a black stroke
+    this.ctx.font = "24pt Impact";
+    this.ctx.fillStyle = "white";
+    this.ctx.fillText( promptText, this.canvas.width / 2, this.canvas.height / 2 + 82);
+    this.ctx.strokeStyle = "black";
+    this.ctx.lineWidth = 2;
+    this.ctx.strokeText( promptText, this.canvas.width / 2, this.canvas.height / 2 + 82);        
+}
+
+
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -23,11 +167,21 @@ var Engine = (function(global) {
         win = global.window,
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
-        lastTime;
+        lastTime,
+        gameTimeElapsed;        
 
+    // set the dimensions of the main canvas and add it to the DOM
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
+
+    // Create the scoreboard BG and FG, and the results screen objects
+    // Each of these has its own canvas that it draws to, which is then
+    // drawn on the main canvas during the render stage.  This was done
+    // for efficiency, as use of fillText was slowing down the game
+    var scoreboardBG = new ScoreboardBG( canvas.width, 50 );
+    var scoreboardFG = new ScoreboardFG( 80, 40 );
+    var resultsScreen = new ResultsScreen( canvas.width, canvas.height );
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
@@ -79,8 +233,10 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
+        if ( gameState === "running" )
+        {
+            updateEntities(dt);
+        }
     }
 
     /* This is called by the update function  and loops through all of the
@@ -95,6 +251,14 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+
+        if ( player.wonGame || player.lostGame )
+        {
+            resultsScreen.setForceDraw();
+            global.gameState = "showResult";
+        }
+
+        gameTimeElapsed += dt;
     }
 
     /* This function initially draws the "game level", it will then call
@@ -136,8 +300,19 @@ var Engine = (function(global) {
             }
         }
 
-
+        // draw the static background for the scoreboard
+        scoreboardBG.draw(ctx, 0, 0);
+        // draw the dynamic content (foreground) for the scorebord 
+        scoreboardFG.draw(ctx, 400, 5);
+        // render the player and enemies
         renderEntities();
+
+        // if the game is over, show the result screen
+        if ( gameState === "showResult" )
+        {
+            // draw the results screen
+            resultsScreen.draw(ctx, 0, 0);
+        }
     }
 
     /* This function is called by the render function and is called on each game
@@ -152,17 +327,50 @@ var Engine = (function(global) {
             enemy.render();
         });
 
+        // render the player as well
         player.render();
     }
 
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
+    /* This function should be called when a new game is started.  It resets
+       the state of all variables needed for the game.
      */
     function reset() {
-        // noop
+        // reset the player (puts him at starting tile)
+        player.reset();
+        // reset the enemies - they will begin off screen to the left
+        allEnemies.forEach(function(enemy) {
+            enemy.reset();
+        });
+        // reset the game clock - I'm not really using this timer for now but good to have
+        gameTimeElapsed = 0;
+        // redraw the background of the scoreboard if the high score changed.  We try to 
+        // draw this as seldom as possible to be more efficient. 
+        if ( currentScore > highScore ) {
+            highScore = currentScore;
+            scoreboardBG.setForceDraw();
+        }
+        // reset the current score to zero
+        currentScore = 0;
+
     }
 
+    /* This function is an additional input handler only takes effect 
+       when the gameState is "showResult" - when the current game is over.
+       It listens for a space key to restart a new game.
+     */
+    function handleInput( keyPressed ) {
+        // if the user pressed the space bar
+        if ( keyPressed === "space" )
+        {
+            // if we're in the "showResult" game state
+            if ( global.gameState === "showResult" )
+            {
+                // reset the game and transition back to the running state
+                reset();
+                global.gameState = "running";
+            }
+        }
+    }
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
      * all of these images are properly loaded our game will start.
@@ -176,9 +384,31 @@ var Engine = (function(global) {
     ]);
     Resources.onReady(init);
 
+
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
+    document.addEventListener('keyup', function(e) {
+        var allowedKeys = {
+            32: 'space',
+        };
+
+        handleInput(allowedKeys[e.keyCode]);
+    });
+
+
+    
     /* Assign the canvas' context object to the global variable (the window
      * object when run in a browser) so that developer's can use it more easily
      * from within their app.js files.
      */
     global.ctx = ctx;
+
+    // keep track of the current gameState.  "running" when game is active, 
+    // "showResult" when the game is over and we are showing the results
+    global.gameState = "running";
+
+    // keep track of both the current and high scores
+    global.currentScore = 0;
+    global.highScore = 53;
+
 })(this);
